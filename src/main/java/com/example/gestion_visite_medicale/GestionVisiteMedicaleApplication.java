436dca8bd946f653571ae0a72748a78b74008a1c
Mihajasoa <mihajasoaalain85@gmail.com>
2025-02-20 11:38:43 +0300

    initiale configuration Project

---
+++ src/main/java/com/example/gestion_visite_medicale/GestionVisiteMedicaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_visite_medicale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVisiteMedicaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVisiteMedicaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestion_visite_medicale/GestionVisiteMedicaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_visite_medicale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVisiteMedicaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
